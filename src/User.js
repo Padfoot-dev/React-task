01dfa41afde96809ff064d3da4e3cbc39fa2c52e
padfoot-dev <abish102001@gmail.com>
2022-11-14 15:26:50 +0530

    initial commit

---
+++ src/User.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Users = ({ user }) => {
+  return (
+    <div className='card-detail'>
+      <img src={user.picture.large} alt="" />
+      <h3>{user.name.first}{user.name.last}</h3>
+    </div>
+  );
+}
+
+export default Users;
